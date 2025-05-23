81ccdaacfdd318736caba70e327ac7c923b449e4
Robotronix <43515689+rtxsc@users.noreply.github.com>
2025-05-23 21:24:00 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+# print(df.columns)
+
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.nama} - Email: {row.emel_alumni} - Grade: {row.tahun_graduasi} ")
+
+# x = st.slider('x')  # 👈 this is a widget
+# st.write(x, 'squared is', x * x)
+
+# chart_data = pd.DataFrame(
+#      np.random.randn(20, 4),
+#      columns=['a', 'b', 'c', 'd'])
+
+# st.line_chart(chart_data)
+
+
+# dataframe = pd.DataFrame(
+#     np.random.randn(10, 20),
+#     columns=('col %d' % i for i in range(20)))
+
+# st.dataframe(dataframe.style.highlight_max(axis=0))
+
+# map_data = pd.DataFrame(
+#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#     columns=['lat', 'lon'])
+
+# st.map(map_data)
+
